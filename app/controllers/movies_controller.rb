ac55e889dc98904c468be8e4cdd6609177f74a69
charliesunliu <cccharliesunliu@gmail.com>
2017-11-12 18:43:39 -0800

    Added API request, review creations, methods for review and movie, and created views for movie, and sorta got devise log in to work a bit

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,2 +1,13 @@
 class MoviesController < ApplicationController
+	def show
+		require 'open-uri'
+		api_key = '3802e4a0fdfbe45bf420d173bfa5b977'
+
+		@obj = JSON.parse(open("https://api.themoviedb.org/3/movie/#{Movie.find(params[:id]).tmdb_id}?api_key=#{api_key}&language=en-US").read)
+		@movie = Movie.find(params[:id])
+	end
+
+	def create
+		#to be implemented
+	end
 end
--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,2 +1,18 @@
 class ReviewsController < ApplicationController
+  def create
+  	movie = Movie.find(params[:id])
+  	review = Review.new
+  	review.user = current_user
+  	review.movie = movie
+  	review.update(review_params)
+  	puts "****************************************************************"
+  	puts review
+  	review.save!
+  	redirect_to movie_path(movie)
+  end
+
+  private
+  def review_params
+  	params.require(:review).permit(:comment, :rating)
+  end
 end
